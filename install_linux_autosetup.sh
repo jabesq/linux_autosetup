51afb61619c253cd6f15ce673af1dbe207627e47
jabesq <jabesq@gmail.com>
2016-02-03 14:34:20 +0100

    Add new alias and needed programs during setup
    This also updates README file
    
    Signed-off-by: jabesq <jabesq@gmail.com>

--- install_linux_autosetup.sh
+++ install_linux_autosetup.sh
@@ -1,15 +1,15 @@
 echo "include ~/linux_autosetup/ALL.nanorc" > ~/.nanorc
 echo "source ~/linux_autosetup/bashrc" > ~/.bashrc
-source ~/.bashrc
 sudo -E add-apt-repository ppa:git-core/ppa -y
 sudo apt-get -qq update 
 sudo apt-get -qq upgrade -y
 sudo apt-get -qq autoremove
 sudo apt-get -qq autoclean
-sudo apt-get -qq install git
+sudo apt-get -qq install git terminator tree diffuse meld ctags
 git --version
-sudo apt-get install terminator
 terminator --version
+tree --version
+
 
 
 sh ~/linux_autosetup/install_awesome_vimrc.sh
