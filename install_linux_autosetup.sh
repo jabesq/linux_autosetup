707689eb8d2fe611b8451bd09d09d2db785d2d7e
jabesq <jabesq@gmail.com>
2016-02-09 11:39:44 +0100

    Update ALLupdate to detect if reboot is needed
    Signed-off-by: jabesq <jabesq@gmail.com>

--- install_linux_autosetup.sh
+++ install_linux_autosetup.sh
@@ -1,18 +1,19 @@
 echo "include ~/linux_autosetup/ALL.nanorc" > ~/.nanorc
 echo "source ~/linux_autosetup/bashrc" > ~/.bashrc
-sudo -E add-apt-repository ppa:git-core/ppa -y
-sudo apt-get -qq update 
-sudo apt-get -qq upgrade -y
-sudo apt-get -qq autoremove
-sudo apt-get -qq autoclean
-sudo apt-get -qq install git terminator tree diffuse meld ctags
+if ! grep -q "git-core/ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+	echo "Updating repository in order to have access to the latest version of Git"
+	sudo -E add-apt-repository ppa:git-core/ppa
+	sudo apt-get -q update
+	sudo apt-get -q upgrade
+fi
+
+sudo apt-get install git terminator tree diffuse meld ctags
 git --version
 terminator --version
 tree --version
 
+sh ~/linux_autosetup/install_awesome_vimrc.sh > /dev/null
 
-
-sh ~/linux_autosetup/install_awesome_vimrc.sh
 
 echo "Please enter Git global username, followed by [ENTER]:"
 read name
@@ -21,4 +22,5 @@ echo "Please enter Git global user mail, followed by [ENTER]:"
 read mail
 
 sed 's/name\ \=/name\ \= \'"$name"'/g' gitconfig | sed 's/email\ \=/email\ \= \'"$mail"'/g' > ~/.gitconfig
+echo "Installed the Linux autosetup configuration successfully! Enjoy :-)"
 
