f6ca3c17dd57b77c0e54fe342759c6df02affbaa
Hugo D. (jabesq) <jabesq@gmail.com>
2016-04-21 18:07:27 +0200

    Add OS detection for install_linux_autosetup script
    Signed-off-by: Hugo D. (jabesq) <jabesq@gmail.com>

--- install_basic_vimrc.sh
+++ install_basic_vimrc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-cd ~/linux_autosetup
-cat ~/linux_autosetup/vimrcs/basic.vim > ~/.vimrc
+cd ${HOME}/linux_autosetup
+cat ${HOME}/linux_autosetup/vimrcs/basic.vim > ~/.vimrc
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
